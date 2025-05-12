50ea13071f0fd3fe75e4aca47b48d0d0dbe0a1a4
Zettex <safaandsafa4@gmail.com>
2025-05-13 00:31:07 +0600

    done

--- src/Days/Day5/AboutMe.jsx
+++ src/Days/Day5/AboutMe.jsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { useRef, useLayoutEffect } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -7,9 +5,9 @@ import SplitText from "gsap/SplitText";
 import KnightModel from "../Day9/KnightModel";
 import Environment from "../Day9/Environment";
 import { FaArrowDown } from "react-icons/fa";
+import ScrollSmoother from "gsap/ScrollSmoother";
 
-gsap.registerPlugin(ScrollTrigger);
-gsap.registerPlugin(SplitText);
+gsap.registerPlugin(SplitText,ScrollTrigger,ScrollSmoother);
 
 function AboutMe() {
   const headerRef = useRef();
@@ -20,8 +18,18 @@ function AboutMe() {
   const imgRef2 = useRef();
   const imgRef3 = useRef();
   const iconRef = useRef();
+  const smoothWrapperRef = useRef()
+  const smoothContentRef = useRef()
 
   useLayoutEffect(() => {
+    const smoother = ScrollSmoother.create({
+      wrapper: smoothWrapperRef.current,
+      content: smoothContentRef.current,
+      smooth:1.5,
+      effects:true
+    })
+
+
     const splitText = new SplitText(headerRef.current, { type: "words" });
     const myText = splitText.words;
     const splitText2 = new SplitText(pRef.current, { type: "chars" });
@@ -125,165 +133,118 @@ function AboutMe() {
 
     return () => {
       if (splitText) splitText.revert();
+      if (smoother) smoother.kill();
       ScrollTrigger.getAll().forEach((t) => t.kill());
     };
   }, []);
 
-  return (
-    <div>
-      <div
-        className="w-full overflow-hidden min-h-screen font-[Syncopate] bg-black px-4 md:p-8 flex flex-col lg:flex-row justify-between meSection"
-        ref={containerRef}
-      >
-        <div className="flex-grow mb-8 lg:mb-0">
-          <div
-            className="uppercase font-semibold text-white w-full mx-auto"
-            ref={headerRef}
-          >
-            <div className="text-lg md:text-xl leading-5">I am</div>
-            <div className="leading-tight text-white uppercase font-bold text-[2rem] sm:text-[2.5rem] md:text-[3.5rem] lg:text-[5rem] xl:text-[5rem] overflow-hidden w-full">
-              Asif Mahmud
+   return (
+    <div id="smooth-wrapper" ref={smoothWrapperRef}>
+      <div id="smooth-content" ref={smoothContentRef}>
+        <div
+          className="w-full overflow-hidden min-h-screen font-[Syncopate] bg-black px-4 md:p-8 flex flex-col lg:flex-row justify-between meSection"
+          ref={containerRef}
+        >
+          <div className="flex-grow mb-8 lg:mb-0">
+            <div className="uppercase font-semibold text-white w-full mx-auto" ref={headerRef}>
+              <div className="text-lg md:text-xl leading-5">I am</div>
+              <div className="leading-tight text-white uppercase font-bold text-[2rem] sm:text-[2.5rem] md:text-[3.5rem] lg:text-[5rem] xl:text-[5rem] overflow-hidden w-full">
+                Asif Mahmud
+              </div>
+            </div>
+
+            <div className="w-full">
+              <div
+                className="text-white w-full md:w-3/4 text-sm sm:text-base md:text-lg font-light leading-5 overflow-hidden font-[Montserrat]"
+                ref={pRef}
+              >
+                I am a fullstack dev. To me, Design is an art. From crafting intuitive user interfaces to building
+                robust backend systems, I bring both structure and soul into every line I write. To me, design is more
+                than color and layout — it's storytelling, emotion, and purpose. I believe great digital experiences
+                live at the intersection of logic and art. Whether it's shaping smooth animations with GSAP, creating
+                seamless APIs with Django, or deploying full-scale apps, I aim to make every project meaningful,
+                impactful, and beautifully functional. Let's build something that speaks.
+              </div>
+              <p className="my-4 text-white">
+                Scroll Below to see what can i do for you{" "}
+                <FaArrowDown className="text-shadow-white mt-3 text-4xl" ref={iconRef} />
+              </p>
             </div>
           </div>
+          <div className="w-full lg:w-[600px] h-[300px] sm:h-[400px] lg:h-full">
+            <KnightModel />
+            <h1 className="text-white text-center">Use Mouse to interact</h1>
+          </div>
+        </div>
 
-          <div className="w-full">
+        <div className="rounded-lg w-full overflow-hidden min-h-screen font-[Syncopate] bg-white px-4 md:p-8 justify-between meSection">
+          <h1 className="text-xl sm:text-2xl">AI integrated apps</h1>
+          <div className="relative bg-white">
             <div
-              className="text-white w-full md:w-3/4 text-sm sm:text-base md:text-lg font-light leading-5 overflow-hidden font-[Montserrat]"
-              ref={pRef}
+              className="w-max absolute text-black top-1/2 left-0 transform -translate-y-1/2 text-4xl sm:text-6xl md:text-8xl font-[Montserrat] uppercase z-0 font-bold whitespace-nowrap"
+              ref={aiRef}
             >
-              I am a fullstack dev. To me, Design is an art. From crafting
-              intuitive user interfaces to building robust backend systems, I
-              bring both structure and soul into every line I write. To me,
-              design is more than color and layout — it's storytelling, emotion,
-              and purpose. I believe great digital experiences live at the
-              intersection of logic and art. Whether it's shaping smooth
-              animations with GSAP, creating seamless APIs with Django, or
-              deploying full-scale apps, I aim to make every project meaningful,
-              impactful, and beautifully functional. Let's build something that
-              speaks.
+              These are my fullstack and AI integrated Apps
             </div>
-            <p className="my-4 text-white">
-              Scroll Below to see what can i do for you{" "}
-              <FaArrowDown
-                className="text-shadow-white mt-3 text-4xl"
-                ref={iconRef}
-              />
-            </p>
-          </div>
-        </div>
-        <div className="w-full lg:w-[600px] h-[300px] sm:h-[400px] lg:h-full">
-          <KnightModel />
-          <h1 className="text-white text-center">Use Mouse to interact</h1>
-        </div>
-      </div>
 
-      <div className="rounded-lg w-full overflow-hidden min-h-screen font-[Syncopate] bg-white px-4 md:p-8 justify-between meSection">
-        <h1 className="text-xl sm:text-2xl">AI integrated apps</h1>
-        <div className="relative bg-white">
-          <div
-            className="w-max absolute text-black top-1/2 left-0 transform -translate-y-1/2 text-4xl sm:text-6xl md:text-8xl font-[Montserrat] uppercase z-0 font-bold whitespace-nowrap"
-            ref={aiRef}
-          >
-            These are my fullstack and AI integrated Apps
-          </div>
+            <div className="relative z-10 pt-[70px] px-2 sm:px-8">
+              <div className="flex justify-around flex-col lg:flex-row gap-8">
+                <a href="https://bracademic.netlify.app/" target="_blank" className="mx-auto" rel="noreferrer">
+                  <div className="w-full sm:w-[400px] md:w-[500px] lg:w-[600px] h-[300px] sm:h-[400px] md:h-[500px] lg:h-[600px] border border-black overflow-hidden">
+                    <img src="./project1.png" alt="project 1" className="w-full h-full object-cover" ref={imgRef1} />
+                  </div>
+                </a>
 
-          <div className="relative z-10 pt-[70px] px-2 sm:px-8">
-            <div className="flex justify-around flex-col lg:flex-row gap-8">
-              <a
-                href="https://bracademic.netlify.app/"
-                target="_blank"
-                className="mx-auto"
-                rel="noreferrer"
-              >
-                <div className="w-full sm:w-[400px] md:w-[500px] lg:w-[600px] h-[300px] sm:h-[400px] md:h-[500px] lg:h-[600px] border border-black overflow-hidden">
-                  <img
-                    src="./project1.png"
-                    alt="project 1"
-                    className="w-full h-full object-cover"
-                    ref={imgRef1}
-                  />
-                </div>
-              </a>
-
-              <a
-                href="https://aisalescoach.netlify.app/"
-                target="_blank"
-                className="mx-auto"
-                rel="noreferrer"
-              >
-                <div className="w-full sm:w-[400px] md:w-[500px] lg:w-[600px] h-[300px] sm:h-[400px] md:h-[500px] lg:h-[600px] border border-black overflow-hidden">
-                  <img
-                    src="./project2.png"
-                    alt="project 1"
-                    className="w-full h-full object-cover"
-                    ref={imgRef2}
-                  />
-                </div>
-              </a>
+                <a href="https://aisalescoach.netlify.app/" target="_blank" className="mx-auto" rel="noreferrer">
+                  <div className="w-full sm:w-[400px] md:w-[500px] lg:w-[600px] h-[300px] sm:h-[400px] md:h-[500px] lg:h-[600px] border border-black overflow-hidden">
+                    <img src="./project2.png" alt="project 1" className="w-full h-full object-cover" ref={imgRef2} />
+                  </div>
+                </a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <div className="rounded-lg w-full overflow-hidden min-h-screen font-[Syncopate] bg-black px-4 md:p-8 justify-between meSection relative">
-        <h1 className="uppercase text-xl sm:text-2xl absolute z-10 text-white">
-          Integrate 3d models with React3 Fiber <br />
-          <span className="font-semibold text-green-400">
-            (Use Mouse to interract)
-          </span>
-        </h1>
-        <Environment />
-      </div>
+        <div className="rounded-lg w-full overflow-hidden min-h-screen font-[Syncopate] bg-black px-4 md:p-8 justify-between meSection relative">
+          <h1 className="uppercase text-xl sm:text-2xl absolute z-10 text-white">
+            Integrate 3d models with React3 Fiber <br />
+            <span className="font-semibold text-green-400">(Use Mouse to interract)</span>
+          </h1>
+          <Environment />
+        </div>
 
-      <div className="w-full rounded-lg overflow-hidden min-h-screen font-sans bg-white text-black px-4 md:p-8 meSection">
-        <h1 className="text-xl sm:text-2xl font-[Syncopate] max-w-5xl mx-auto">
-          We can work together
-        </h1>
-        <div className="max-w-5xl mx-auto my-6 md:my-12 flex flex-col lg:flex-row justify-between gap-8 items-center">
-          <div className="w-full lg:w-[500px] h-[300px] sm:h-[400px] md:h-[500px] lg:h-[600px] overflow-hidden rounded-md mx-auto">
-            <img
-              src="./dp.jpg"
-              alt="profile"
-              className="w-full h-full object-cover"
-              ref={imgRef3}
-            />
-          </div>
-          <div className="text-lg sm:text-xl md:text-2xl flex justify-between  lg:flex-col space-x-8">
-            <div>
-              <h1 className="font-semibold text-xl sm:text-2xl mb-3">
-                My Stack:
-              </h1>
-              <p>
-                <span className="text-blue-700 font-semibold">Frontend:</span>{" "}
-                React, NextJs
-              </p>
-              <p>
-                <span className="text-blue-700 font-semibold">Backend:</span>{" "}
-                Django, DRF
-              </p>
-              <p>
-                <span className="text-blue-700 font-semibold">Animation:</span>{" "}
-                GSAP, R3F
-              </p>
+        <div className="w-full rounded-lg overflow-hidden min-h-screen font-sans bg-white text-black px-4 md:p-8 meSection">
+          <h1 className="text-xl sm:text-2xl font-[Syncopate] max-w-5xl mx-auto">We can work together</h1>
+          <div className="max-w-5xl mx-auto my-6 md:my-12 flex flex-col lg:flex-row justify-between gap-8">
+            <div className="w-full lg:w-[500px] h-[300px] sm:h-[400px] md:h-[500px] lg:h-[600px] overflow-hidden rounded-md mx-auto">
+              <img src="./dp.jpg" alt="profile" className="w-full h-full object-cover" />
             </div>
-            <div className="lg:mt-6">
-              <h1 className="font-semibold text-xl sm:text-2xl mb-3 lg:mt-6">
-                Contact Me:
-              </h1>
+            <div className="text-lg sm:text-xl md:text-2xl">
+              <h1 className="font-semibold text-2xl sm:text-3xl md:text-4xl mb-3">My Stack:</h1>
               <p>
-                <span className="text-blue-700 font-semibold">Cell:</span>{" "}
-                +8801317482129
+                <span className="text-blue-700 font-semibold">Frontend:</span> React, NextJs
               </p>
               <p>
-                <span className="text-blue-700 font-semibold">Email:</span>{" "}
-                safaandsafa4@gmail.com
+                <span className="text-blue-700 font-semibold">Backend:</span> Django, DRF
               </p>
+              <p>
+                <span className="text-blue-700 font-semibold">Animation:</span> GSAP, R3F
+              </p>
+              <div className="mt-6">
+                <h1 className="font-semibold text-xl sm:text-2xl mb-3 mt-6">Contact Me:</h1>
+                <p>
+                  <span className="text-blue-700 font-semibold">Cell:</span> +8801317482129
+                </p>
+                <p>
+                  <span className="text-blue-700 font-semibold">Email:</span> safaandsafa4@gmail.com
+                </p>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
 export default AboutMe;
